4bfd109f2e9779a8773f20abb3ce49810d013cb5
jonathan <jonathan.123kutumbakana@gmail.com>
2024-03-20 13:56:28 +0300

    DONE

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        self.word_list = word_list
+
+        return [element for element in self.word_list if sorted(self.word) == sorted(element) ]
